c183e4353e57cfae834603839f27d5959552c600
tomatko <tomatko22@gmail.com>
2018-12-13 12:41:46 +0300

    убирает подключение лишних плагинов

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,5 @@
 "use strict";
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-cwebp');
-  grunt.loadNpmTasks('grunt-posthtml');
-  grunt.loadNpmTasks('grunt-uncss');
-  grunt.loadNpmTasks('grunt-svgstore');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
